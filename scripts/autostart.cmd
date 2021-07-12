390bc72546caddda4787bd5dac5d46226e91c706
RH <incredincomp@gmail.com>
2021-07-12 11:00:40 -0700

    quiet vcredist

--- scripts/autostart.cmd
+++ scripts/autostart.cmd
@@ -8,8 +8,8 @@ msiexec /i "%TEMP%\corretto.msi" /qn /norestart
 "%TEMP%\dosbox.exe" /S
 "%TEMP%\notepad++.exe" /S
 "%TEMP%\sublime.exe" /VERYSILENT /NORESTART /TASKS="contextentry"
-"%TEMP%\vcredist_x64.exe"
-"%PROGRAMFILES%\7-Zip\7z.exe" x "%TEMP%\ghidra.zip" -o"%USERPROFILE%\Desktop\ghidra" 
+"%TEMP%\vcredist_x64.exe" /passive /quiet
+"%PROGRAMFILES%\7-Zip\7z.exe" x "%TEMP%\ghidra.zip" -o"%USERPROFILE%\Desktop\ghidra"
 "%PROGRAMFILES%\7-Zip\7z.exe" x "%TEMP%\sysinternals.zip" -o"%USERPROFILE%\Desktop\sysinternals"
 "%PROGRAMFILES%\7-Zip\7z.exe" x "%TEMP%\x64dbg.zip" -o"%USERPROFILE%\Desktop\x64dbg"
 "%PROGRAMFILES%\7-Zip\7z.exe" x "%TEMP%\dnSpy.zip" -o"%USERPROFILE%\Desktop\dnSpy"
